133f617eedc35f2558ef188d4146f750029f8496
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-01 13:52:51 -0300

    fix typo on rentals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_163416) do
+ActiveRecord::Schema.define(version: 2021_06_01_165234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define(version: 2021_06_01_163416) do
     t.integer "bathroom"
     t.integer "bedroom"
     t.integer "garage"
-    t.integer "squqreft"
     t.bigint "user_id", null: false
     t.bigint "address_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "main_photo"
+    t.integer "squareft"
     t.index ["address_id"], name: "index_rentals_on_address_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
