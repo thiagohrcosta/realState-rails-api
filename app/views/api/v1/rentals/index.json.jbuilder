03dbcce38336a386bae15daaf12cab8c8f3a05a2
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-01 13:51:12 -0300

    Add seeds to test API endpoint

--- app/views/api/v1/rentals/index.json.jbuilder
+++ app/views/api/v1/rentals/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.array! @rentals do |rental|
-  json.extract! rental, :id, :title, :address_id, :price, :bathroom, :bedroom, :garage, :squareft
+  json.extract! rental, :id, :title, :address, :price, :bathroom, :bedroom, :garage, :squqreft
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+puts "Destroy all previous db";
+
+Address.destroy_all
+Rental.destroy_all
+User.destroy_all
+
+puts "Add user..."
+
+User.create!(
+  email: "thiago@costa.com",
+  password: "123456"
+)
+
+puts "Adding new database............"
+
 Address.create!(
   street: "Rua dos Goitacazes",
   number: "292",
@@ -22,3 +37,44 @@ Address.create!(
   country: "Brazil"
 )
 
+puts "Address created..."
+
+puts "Add new rental to db..."
+
+Rental.create!(
+  title: "Apartamento montado",
+  address_id: 1,
+  user_id: 1,
+  price: 2000,
+  bathroom: 2,
+  bedroom: 3,
+  garage: 2,
+  squqreft: 200,
+  main_photo: "https://res.cloudinary.com/dloadb2bx/image/upload/v1622565610/photo-1522708323590-d24dbb6b0267_tf7w1b.jpg"
+)
+
+
+Rental.create!(
+  title: "Próximo ao centro",
+  address_id: 2,
+  user_id: 1,
+  price: 2500,
+  bathroom: 2,
+  bedroom: 2,
+  garage: 1,
+  squqreft: 230,
+  main_photo: "https://res.cloudinary.com/dloadb2bx/image/upload/v1622565782/photo-1515263487990-61b07816b324_klurur.jpg"
+)
+
+
+Rental.create!(
+  title: "Melhor vista da cidade",
+  address_id: 3,
+  user_id: 1,
+  price: 3500,
+  bathroom: 3,
+  bedroom: 3,
+  garage: 2,
+  squqreft: 260,
+  main_photo: "https://res.cloudinary.com/dloadb2bx/image/upload/v1622566100/photo-1494161402158-1b81a9a904bb_anuzr8.jpg"
+)
