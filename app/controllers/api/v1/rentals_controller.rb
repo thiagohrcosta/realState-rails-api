54bb50738e8e265d4f1273db0675506008ffba2a
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-01 15:15:22 -0300

    Updated API complete

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::RentalsController < Api::V1::BaseController
-  before_action :set_rental, only: [:show]
+  before_action :set_rental, only: [:show, :update]
 
   def index
     @rentals = policy_scope(Rental)
     authorize @rentals
+  end
+
+  def update
+    if @rental.update(rental_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:title, :price, :bathroom, :bedroom, :garage, :address_id, :main_photo, :squareft)
   end
 
   def set_rental
